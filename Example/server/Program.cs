905a9b42247e2bdcd2f623ed1f25a5f9782fee5c
Ernst Naezer <ernstnaezer@gmail.com>
2011-09-02 20:34:02 +0200

    Added some code to demonstrate message buffering

--- Example/server/Program.cs
+++ Example/server/Program.cs
@@ -2,25 +2,31 @@
 {
     using System;
     using Ultralight;
+    using Ultralight.Client;
     using Ultralight.Listeners;
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            var wsListener = new StompWsListener(new Uri("ws://localhost:8181/"));
+            var address = new Uri("ws://localhost:8181/");
+            var wsListener = new StompWsListener(address);
 
-            wsListener
-                .OnConnect += client =>
-                                  {
-                                      Console.WriteLine("a new client connected!");
-                                      client.OnMessage +=
-                                          msg =>
-                                          Console.Out.WriteLine("msg received: {0} {1}", msg.Command, msg.Body);
-                                  };
+            wsListener.OnConnect
+                += stompClient =>
+                       {
+                           Console.WriteLine("a new client connected!");
+                           stompClient.OnMessage +=
+                               msg =>
+                               Console.Out.WriteLine("msg received: {0} {1}", msg.Command, msg.Body);
+                       };
 
             var server = new StompServer(wsListener);
             server.Start();
+
+            var client = new StompClient();
+            client.Connect(address);
+            client.Send("/queue/test", "hi there. you are the first to connect");
 
             Console.Out.WriteLine("Press [Enter] to stop the server");
             Console.ReadLine();
