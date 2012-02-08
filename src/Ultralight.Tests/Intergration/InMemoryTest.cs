c2809a92c0e96f0fe78408ae589dbd7475a99329
enix <ernstnaezer@gmail.com>
2012-02-08 21:12:18 +0100

    added license info

--- src/Ultralight.Tests/Intergration/InMemoryTest.cs
+++ src/Ultralight.Tests/Intergration/InMemoryTest.cs
@@ -1,4 +1,17 @@
-﻿namespace Ultralight.Tests.Intergration
+﻿// Copyright 2011 Ernst Naezer, et. al.
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed 
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
+
+namespace Ultralight.Tests.Intergration
 {
     using System;
     using System.Linq;
@@ -12,17 +25,17 @@
     public class InMemoryTest
     {
         private StompServer _server;
-        private StompClient _client;
+        private StompClient _client1;
 
         [SetUp]
         public void Setup()
         {
             var inMemoryListener = new StompInMemoryListener();
             _server = new StompServer(inMemoryListener);
-            _client = new StompClient(new InMemoryTransport(inMemoryListener));
+            _client1 = new StompClient(new InMemoryTransport(inMemoryListener));
 
             _server.Start();
-            _client.Connect();
+            _client1.Connect();
         }
 
         [Test]
@@ -30,7 +43,7 @@
         {
             Assert.That(_server.Queues, Is.Empty);
 
-            _client.Send("123", "foo:bar");
+            _client1.Send("123", "foo:bar");
 
             Thread.Sleep(TimeSpan.FromSeconds(3));
 
